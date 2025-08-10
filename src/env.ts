58ef6819f69d51ece26c0833917b5fa5a1fe60ad
Jacob Chademwiri <jchademwiri@gmail.com>
2025-08-10 03:56:02 +0200

    chore: add @t3-oss/env-nextjs dependency to package.json and pnpm-lock.yaml
    - Added the @t3-oss/env-nextjs package with version ^0.13.8 to package.json for enhanced environment management in Next.js applications.
    - Updated pnpm-lock.yaml to include the new dependency and its peer dependencies, ensuring compatibility with existing packages.

---
+++ src/env.ts
@@ -0,0 +1,31 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    BETTER_AUTH_SECRET: z.string().min(1, {
+      message: 'BETTER_AUTH_SECRET is required',
+    }),
+    BETTER_AUTH_URL: z.string().url(),
+    RESEND_API_KEY: z
+      .string()
+      .min(1, { message: 'RESEND_API_KEY is required' }),
+    UPLOADTHING_TOKEN: z
+      .string()
+      .min(1, { message: 'UPLOADTHING_TOKEN is required' }),
+  },
+  client: {
+    NEXT_PUBLIC_URL: z.string().min(1, {
+      message: 'NEXT_PUBLIC_URL is required',
+    }),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
+});
